d129770933fc6abf55687197d8f2df5b270fdf44
Alvaro Fidalgo <afidalgo@stratio.com>
2015-09-02 13:22:42 +0200

    add cassandra interpreter structure

---
+++ cassandra/src/test/java/com/stratio/cassandra/CassandraInterpreterTest.java
@@ -0,0 +1,17 @@
+package com.stratio.cassandra;
+
+
+import org.junit.Test;
+
+/**
+ * Created by stratio on 1/09/15.
+ */
+public class CassandraInterpreterTest {
+
+
+
+    @Test public void firstTest(){
+
+    }
+
+}
