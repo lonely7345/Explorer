7280a753c63b8dd87fea06b3eb2b9607aa1d35de
alvarofidalgo <dmj200@gmail.com>
2015-09-30 17:42:51 +0200

    add exception cause message

--- notebook-engine/src/main/java/com/stratio/notebook/reader/PropertiesReader.java
+++ notebook-engine/src/main/java/com/stratio/notebook/reader/PropertiesReader.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
