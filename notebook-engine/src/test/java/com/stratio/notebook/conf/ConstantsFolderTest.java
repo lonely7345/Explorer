f00cb3bf0996e01d0233b8388b641a3e5942cd11
alvarofidalgo <dmj200@gmail.com>
2015-09-30 12:08:31 +0200

    Revert "solve problems with rebase"
    This reverts commit 4fae6f8eaa90cf3f5f68376122b904a8806873e9.

---
+++ notebook-engine/src/test/java/com/stratio/notebook/conf/ConstantsFolderTest.java
@@ -0,0 +1,23 @@
+package com.stratio.notebook.conf;
+
+
+import org.junit.Test;
+
+
+//TODO :  LA RUTA DONDE ESTÁN LAS CONFIGURACIONES EN LA INSTALACIÓN SON
+//TODO : /etc/sds/notebook
+public class ConstantsFolderTest {
+
+
+
+    @Test
+    public void whenExecuteWithCode(){
+
+    }
+
+
+    @Test public void whenExecuteWithInstallation(){
+
+    }
+
+}
